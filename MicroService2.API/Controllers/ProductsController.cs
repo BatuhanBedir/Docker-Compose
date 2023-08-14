737d204b8a708faf814690278d04594905f2a6c6
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-14 10:41:12 +0300

    twoServiceCommunications

---
+++ MicroService2.API/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService2.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { id = 1, name = "Pencil2(Microservice2)" });
+    }
+}
